aa75d89c711b9473be7810390734214ca084ce48
grimfang4@gmail.com <grimfang4@gmail.com@f69afed9-71f0-c812-a9e8-761ac3947e42>
2012-10-21 23:50:14 +0000

    Added sample project which includes bones (bones not implemented yet in SCMLpp).
    git-svn-id: http://scml-pp.googlecode.com/svn/trunk@22 f69afed9-71f0-c812-a9e8-761ac3947e42

--- source/main.cpp
+++ source/main.cpp
@@ -9,6 +9,7 @@ using namespace std;
 int main(int argc, char* argv[])
 {
     SCML::Data data("samples/monster/Example.SCML");
+    //SCML::Data data("samples/knight/knight.scml");
     data.log();
     
     if(!init(800, 600))
